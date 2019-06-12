4b8c3bebc86afee222b0a5854ba5f33acf1680fc
Tausif ali <ajaxhis@tutanota.com>
2019-06-12 13:20:33 +0530

    added resource structure and interface

--- include/ResourceManager.hpp
+++ include/ResourceManager.hpp
@@ -4,15 +4,17 @@
 #include <SFML/Graphics.hpp>
 #include <unordered_map>
 
-
-/**
- * * we are going to have separate maps for texture and font resource
- * ? or should it be made to have single map with a simple resource interface 
- */
 class ResourceManager
 {
 public:
+    ResourceManager();
+    ~ResourceManager();
+
+    sf::Texture &getTexture(const unsigned int ID) const;
+    unsigned int loadTexture(const char *path);
+
 private:
-std::unordered_map<unsigned int,std::unique_ptr<sf::Texture>> _resourceMap;
-std::unordered_map<unsigned int,std::unique_ptr<sf::Font>> _fontMap;
+    // Using unordered map as it provides 'near' O(1) lookup
+    std::unordered_map<unsigned int, std::unique_ptr<sf::Texture>> _resourceMap;
+    static unsigned int id;
 };
--- src/ResourceManager.cpp
+++ src/ResourceManager.cpp
@@ -1,2 +1,23 @@
 #include "ResourceManager.hpp"
 
+unsigned int ResourceManager::id = 0;
+
+ResourceManager::ResourceManager()
+{
+}
+ResourceManager::~ResourceManager()
+{
+}
+
+unsigned int ResourceManager::loadTexture(const char *path)
+{
+    std::unique_ptr<sf::Texture> tex;
+    if(tex->loadFromFile(path))
+    {
+        _resourceMap.emplace(id++,tex);
+    }
+    
+}
+sf::Texture &ResourceManager::getTexture(const unsigned int ID) const
+{
+}
