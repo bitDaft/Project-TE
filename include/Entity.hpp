ee718f5e8817eb96b96d9e8ae0f8979160ece05a
Tausif ali <ajaxhis@tutanota.com>
2019-07-02 02:49:19 +0530

    added reactionmanager instance to entity baseclass

--- include/Entity.hpp
+++ include/Entity.hpp
@@ -4,30 +4,27 @@
  * Created Date: Tuesday July 2nd 2019
  * Author: bitDaft
  * -----
- * Last Modified: Tuesday July 2nd 2019 2:22:38 am
+ * Last Modified: Tuesday July 2nd 2019 2:48:59 am
  * Modified By: bitDaft at <ajaxhis@tutanota.com>
  * -----
  * Copyright (c) 2019 bitDaft coorp.
  */
 
-
 #ifndef ENTITY_HPP
 #define ENTITY_HPP
 
-#include "ActionMapper.hpp"
+#include "ReactionMapper.hpp"
 #include <stack>
 #include <SFML/Window/Event.hpp>
 
 class Entity : private sf::NonCopyable
 {
 public:
+  ReactionMapper *react;
   Entity();
   ~Entity();
 
 private:
-
 };
 
 #endif
-
-
