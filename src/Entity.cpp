f53d7fbd6b636a708603d8919e9449bc72e60705
Tausif ali <ajaxhis@tutanota.com>
2019-07-02 02:22:52 +0530

    added base entity class

---
+++ src/Entity.cpp
@@ -0,0 +1,16 @@
+/*
+ * File: Entity.cpp
+ * Project: Project-TE
+ * Created Date: Tuesday July 2nd 2019
+ * Author: bitDaft
+ * -----
+ * Last Modified: Tuesday July 2nd 2019 2:22:38 am
+ * Modified By: bitDaft at <ajaxhis@tutanota.com>
+ * -----
+ * Copyright (c) 2019 bitDaft coorp.
+ */
+
+#include "Entity.hpp"
+
+Entity::Entity() {}
+Entity::~Entity() {}
