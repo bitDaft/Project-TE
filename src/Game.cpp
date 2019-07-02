f373e35eb292cbe7b443b5b6b792028338910ba1
Tausif ali <ajaxhis@tutanota.com>
2019-07-02 10:01:25 +0530

    integrated input with Game with a test example

--- src/Game.cpp
+++ src/Game.cpp
@@ -4,34 +4,59 @@
  * Created Date: Sunday June 9th 2019
  * Author: bitDaft
  * -----
- * Last Modified: Tuesday July 2nd 2019 2:38:07 am
+ * Last Modified: Tuesday July 2nd 2019 9:56:33 am
  * Modified By: bitDaft at <ajaxhis@tutanota.com>
  * -----
  * Copyright (c) 2019 bitDaft coorp.
  */
 
 #include "Game.hpp"
+#include <iostream>
 
 #define DEFAULT_FRAME_RATE (1.f / 120.f)
 #define DEFAULT_SCREEN_WIDTH 640
 #define DEFAULT_SCREEN_HEIGHT 480
 #define DEFAULT_GAME_NAME ("Application 1")
 
+bool Game::moveu(sf::RenderWindow &, sf::Event &)
+{
+    player.move({0.f, -1.f});
+    return false;
+}
+bool Game::moved(sf::RenderWindow &, sf::Event &)
+{
+    player.move({0.f, 1.f});
+    return false;
+}
+bool Game::movel(sf::RenderWindow &, sf::Event &)
+{
+    player.move({-1.f, 0.f});
+    return false;
+}
+bool Game::mover(sf::RenderWindow &, sf::Event &)
+{
+    player.move({1.f, 0.f});
+    return false;
+}
 // Default constructor
 Game::Game()
     : gameWindow(sf::VideoMode(DEFAULT_SCREEN_WIDTH, DEFAULT_SCREEN_HEIGHT), DEFAULT_GAME_NAME),
       timePerFrame(sf::seconds(DEFAULT_FRAME_RATE)),
-      isRunning(true), player(), textureManager(), handles(), inpMan(this)
+      isRunning(true), player(), textureManager(), handles(), _inputManager(this), _aMapper(), _rMapper(gameWindow, this)
 {
+    _reactionMapper = &_rMapper;
     gameWindow.setKeyRepeatEnabled(false);
+    _inputManager.setActionMapper(&_aMapper);
 }
 // Constructor
 Game::Game(const int w, const int h, const char *n)
     : gameWindow(sf::VideoMode(w, h), n),
       timePerFrame(sf::seconds(DEFAULT_FRAME_RATE)),
-      isRunning(true), player(), textureManager(), handles(), inpMan(this)
+      isRunning(true), player(), textureManager(), handles(), _inputManager(this), _aMapper(), _rMapper(gameWindow, this)
 {
+    _reactionMapper = &_rMapper;
     gameWindow.setKeyRepeatEnabled(false);
+    _inputManager.setActionMapper(&_aMapper);
 }
 
 void Game::setWindowSize(const int width, const int height)
@@ -116,6 +141,10 @@ void Game::processEvents()
                 isRunning = false;
                 gameWindow.close();
             }
+            else
+            {
+                _inputManager.processInputsEvent(event);
+            }
 
             break;
         case sf::Event::KeyReleased:
@@ -132,6 +161,14 @@ void Game::init()
     unsigned int playerTexture = textureManager.loadTexture("assets/player.png");
     handles.insert(std::make_pair(TEXTURE::PLAYER, playerTexture));
     player.setTexture(textureManager.getTexture(playerTexture));
+    _aMapper.bindInputToAction(sf::Keyboard::Down, 1);
+    _aMapper.bindInputToAction(sf::Keyboard::Up, 2);
+    _aMapper.bindInputToAction(sf::Keyboard::Left, 3);
+    _aMapper.bindInputToAction(sf::Keyboard::Right, 4);
+    _reactionMapper->bindActionToReaction<moved>(1);
+    _reactionMapper->bindActionToReaction<moveu>(2);
+    _reactionMapper->bindActionToReaction<movel>(3);
+    _reactionMapper->bindActionToReaction<mover>(4);
 }
 
 void Game::update(const sf::Time dt)
