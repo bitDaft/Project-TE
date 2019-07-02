f3491325a5e2e89d62e3bfedc20ddbc8a4fd0c19
Tausif ali <ajaxhis@tutanota.com>
2019-07-02 10:00:33 +0530

    clean up and added new ptr to hold game obj

--- include/InputManager.hpp
+++ include/InputManager.hpp
@@ -4,7 +4,7 @@
  * Created Date: Tuesday July 2nd 2019
  * Author: bitDaft
  * -----
- * Last Modified: Tuesday July 2nd 2019 2:45:35 am
+ * Last Modified: Tuesday July 2nd 2019 9:51:30 am
  * Modified By: bitDaft at <ajaxhis@tutanota.com>
  * -----
  * Copyright (c) 2019 bitDaft coorp.
@@ -25,26 +25,16 @@ public:
 
   void setActionMapper(ActionMapper *);
   void processInputsRealtime();
-  void processInputsEvent(sf::Event::KeyEvent &);
-  // template <auto T>
-  // void bindActionToReaction(unsigned int action)
-  // {
-  //   using className = typename point_to_mem<decltype(T)>::class_type;
-  //   className *tthis = static_cast<className *>(objThis);
-  //   _reactionMap[action] = delegate::from_method<className, T>(tthis);
-  // }
-  // void bindActionToReaction(unsigned int action, delegate callback);
-  // void executeCallback(unsigned int action, sf::Event &event);
+  void processInputsEvent(sf::Event &);
 
-  // void clearBinding(unsigned int key);
-
-  // void clearAllBinding();
+  void pushEntity(Entity *);
+  void replaceEntity(Entity *);
+  void popEntity();
 
 private:
-  // it has a actionmapper obj
-  // it contains a stack
   ActionMapper *_actionMapper;
-  std::vector<std::shared_ptr<Entity>> _itemList;
+  std::vector<Entity *> _itemList;
+  Entity *gameEntity;
 };
 
 #endif
--- src/InputManager.cpp
+++ src/InputManager.cpp
@@ -4,7 +4,7 @@
  * Created Date: Tuesday July 2nd 2019
  * Author: bitDaft
  * -----
- * Last Modified: Tuesday July 2nd 2019 2:45:35 am
+ * Last Modified: Tuesday July 2nd 2019 9:51:30 am
  * Modified By: bitDaft at <ajaxhis@tutanota.com>
  * -----
  * Copyright (c) 2019 bitDaft coorp.
@@ -12,18 +12,38 @@
 
 #include "InputManager.hpp"
 
-InputManager::InputManager(Entity *game) : _actionMapper(0), _itemList() {
-  _itemList.push_back(std::make_shared<Entity>(game));
+InputManager::InputManager(Entity *game) : _actionMapper(0), _itemList()
+{
+  gameEntity = game;
 }
 InputManager::~InputManager() {}
 void InputManager::setActionMapper(ActionMapper *aMap)
 {
   _actionMapper = aMap;
 }
+void InputManager::pushEntity(Entity *e)
+{
+  _itemList.push_back(e);
+}
+void InputManager::replaceEntity(Entity *e)
+{
+  popEntity();
+  pushEntity(e);
+}
+void InputManager::popEntity()
+{
+  _itemList.pop_back();
+}
 void InputManager::processInputsRealtime() {}
-void InputManager::processInputsEvent(sf::Event::KeyEvent &e) {
-  unsigned int t = _actionMapper->getBoundAction(e.code);
-  for(auto entity : _itemList)
+void InputManager::processInputsEvent(sf::Event &e)
+{
+  unsigned int t = _actionMapper->getBoundAction(e.key.code);
+  for (auto entity : _itemList)
   {
+    if (!(entity->_reactionMapper->executeCallback(t, e)))
+    {
+      break;
+    }
   }
+  gameEntity->_reactionMapper->executeCallback(t, e);
 }
