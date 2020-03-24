4c36c5bd0f1d8cf2530d15e36967694d7a20dca4
Tausif Ali <ajaxhis@tutanota.com>
2020-03-24 22:52:24 +0530

    added comments and minor changes for linux build

--- include/InputHandler.hpp
+++ include/InputHandler.hpp
@@ -4,7 +4,7 @@
  * Created Date: Tuesday July 2nd 2019
  * Author: bitDaft
  * -----
- * Last Modified: Tuesday, March 24th 2020 10:14:18 am
+ * Last Modified: Tuesday, March 24th 2020 1:45:13 pm
  * Modified By: bitDaft at (ajaxhis@tutanota.com>)
  * -----
  * Copyright (c) 2019 bitDaft coorp.
@@ -30,6 +30,8 @@ public:
   // ?check out why it is public and why it cant be made private
   // TODO: create a delegate function for the needed function from this object
   // ^ the inputmanager needs it to be public so it can call the executecallback function
+  // ? can we make this a friend of inputmanager so as to be able to access it
+  // ^ Nah.. i think this is fine. if the y break. they break;
   ReactionMapper *_reactionMapper;
 };
 
